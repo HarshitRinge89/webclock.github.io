a1c7fc5b70adc4cae8b1cc9835bedc4bad59809b
Harshit Ringe <harshit.ringe@gmail.com>
2025-08-23 17:16:36 +0530

    Update time.js

--- time.js
+++ time.js
@@ -3,42 +3,43 @@ function startTime(){
   let h=today.getHours();
   let m=today.getMinutes();
   let s=today.getSeconds();
+  let day=today.getDay();
+  let date=today.getDate();
+  let month=today.getMonth();
+  let year=today.getFullYear();
+  let utch = today.getUTCHours();
+  let utcm = today.getUTCMinutes();
+  let utcs = today.getUTCSeconds();
   m = checkTime(m);
   s = checkTime(s);
+  utcm=checkTime(utcm);
+  utcs=checkTime(utcs);
   document.getElementById('time').innerHTML =  h + ":" + m + ":" + s;
+  switch (day) {
+    case 0:day = "Sunday";break;
+    case 1:day = "Monday";break;
+    case 2:day = "Tuesday";break;
+    case 3:day = "Wednesday";break;
+    case 4:day = "Thursday";break;
+    case 5:day = "Friday";break;
+    case 6:day = "Saturday";break;
+  }
+  month+=1;
+  document.getElementById('dayndate').innerHTML= date + "-" + month + "-" + year + "," + day;
+  document.getElementById('utc').innerHTML = "The UTC Time is: "+  utch + ":" + utcm + ":" + utcs;
   setTimeout(startTime, 1000);
 }
-function checkTime(i) {
-  if(i<10){i="0"+i};
-  return i;
-}
+function checkTime(i) {if(i<10){i="0"+i};return i;}
 function checkDate(){
   const tday= new Date();
   let date= tday.getFullYear();
   document.getElementById('date').innerHTML = date;
 }
-
-/*Analog */
-
-// function analog() {
-//   let now = new Date();
-//   let seconds= now.getSeconds();
-//   let minutes= now.getMinutes();
-//   let hours=now.getHours();
-//   let secdeg=seconds*6;
-//   let mindeg= minutes*6;
-//   let hourdeg= 30*hours + (0.5 * minutes);
-//   setTimeout(startTime, 1000);
-//   hourhand.style.transform =`translate(-50%,-100%) rotate(${hourdeg}deg)`
-//   minutehand.style.transform =`translate(-50%,-100%) rotate(${mindeg}deg)`
-//   secondhand.style.transform =`translate(-50%,-85%) rotate(${secdeg}deg)`
-//   setTimeout(startTime, 1000);
-// }
 setInterval(() => {
-  let now = new Date()
-  let hours = now.getHours()
-  let minutes = now.getMinutes()
-  let seconds = now.getSeconds()
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
   let hourdeg = (30 * hours) + (0.5 * minutes);
   let mindeg = 6 * minutes;
   let secdeg = 6 * seconds;
@@ -49,11 +50,6 @@ setInterval(() => {
 function displayElement() {
   let analog = document.getElementById("clock");
   let digi=document.getElementById("digiclk");
-  if (digi.style.display !== "none") {
-        digi.style.display = "none";
-        analog.style.display = "block";
-      } else {
-        digi.style.display = "block";
-        analog.style.display = "none";
-      }
+  if (digi.style.display !== "none") {digi.style.display = "none";analog.style.display = "block";}
+  else {digi.style.display = "block";analog.style.display = "none";}
 }
